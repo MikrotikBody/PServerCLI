8dbe3b20ac94dff706647b52a507843887267f36
kokspflanze <kokspflanze@gmx.de>
2014-11-22 15:35:00 +0100

    init

--- src/PServerCLI/Module.php
+++ src/PServerCLI/Module.php
@@ -5,15 +5,16 @@ namespace PServerCLI;
 use Zend\ModuleManager\Feature\ServiceProviderInterface;
 
 class Module implements ServiceProviderInterface {
+
 	public function getConfig() {
-		return include __DIR__ . '/config/module.config.php';
+		return include __DIR__ . '/../../config/module.config.php';
 	}
 
 	public function getAutoloaderConfig() {
 		return array(
 			'Zend\Loader\StandardAutoloader' => array(
 				'namespaces' => array(
-					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+					__NAMESPACE__ => __DIR__ . '/../../src/' . __NAMESPACE__,
 				),
 			),
 		);
