4afd6379e338345c1df1784a9e12bacc2bb69555
kokspflanze <kokspflanze@gmx.de>
2015-02-01 00:31:59 +0100

    player history via CLI

--- config/module.config.php
+++ config/module.config.php
@@ -1,2 +1,25 @@
 <?php
-return array();
+return [
+	'controllers' => [
+		'invokables' => [
+			'PServerCLI\Controller\PlayerHistory' => 'PServerCLI\Controller\PlayerHistoryController',
+		]
+	],
+
+	'console' => [
+		'router' => [
+			'routes' => [
+				'get-happen-use' => [
+					'options' => [
+						'route' => 'player-history <player>',
+						'defaults' => [
+							'__NAMESPACE__' => 'PServerCLI\Controller',
+							'controller' => 'PlayerHistory',
+							'action' => 'index'
+						],
+					],
+				],
+			]
+		]
+	],
+];
--- src/PServerCLI/Controller/PlayerHistoryController.php
+++ src/PServerCLI/Controller/PlayerHistoryController.php
@@ -8,11 +8,35 @@
 
 namespace PServerCLI\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Mvc\Controller\AbstractActionController,
+    Zend\Console\Request as ConsoleRequest;
 
 class PlayerHistoryController extends AbstractActionController {
 
-	public function indexAction(){
+	/** @var  \PServerCMS\Service\PlayerHistory */
+	protected $playerHistoryService;
 
+	public function indexAction(){
+		$request = $this->getRequest();
+
+		// only allowed from console....
+		if (!$request instanceof ConsoleRequest){
+			throw new \RuntimeException('You can only use this action from a console!');
+		}
+
+		$player = $request->getParam('player',0);
+
+		$this->getPlayerHistory()->setCurrentPlayer($player);
+	}
+
+	/**
+	 * @return \PServerCMS\Service\PlayerHistory
+	 */
+	protected function getPlayerHistory(){
+		if (!$this->playerHistoryService) {
+			$this->playerHistoryService = $this->getServiceLocator()->get('pserver_playerhistory_service');
+		}
+
+		return $this->playerHistoryService;
 	}
 } 
