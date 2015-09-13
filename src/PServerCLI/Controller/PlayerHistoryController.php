97ad7d3d7943998648bb8b2670c8b6f26cc9698c
KoKsPfLaNzE <kokspflanze@gmx.de>
2015-09-13 23:30:26 +0200

    player-history run only 1 time edit composer.json

--- src/PServerCLI/Controller/PlayerHistoryController.php
+++ src/PServerCLI/Controller/PlayerHistoryController.php
@@ -4,15 +4,16 @@ namespace PServerCLI\Controller;
 
 use PServerCMS\Helper\HelperService;
 use PServerCMS\Helper\HelperServiceLocator;
-use Zend\Mvc\Controller\AbstractActionController,
-    Zend\Console\Request as ConsoleRequest;
+use Symfony\Component\Filesystem\LockHandler;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Console\Request as ConsoleRequest;
 
 class PlayerHistoryController extends AbstractActionController
 {
-    use HelperServiceLocator, HelperService;
+	use HelperServiceLocator, HelperService;
 
 	public function indexAction()
-    {
+	{
 		$request = $this->getRequest();
 
 		// only allowed from console....
@@ -20,7 +21,15 @@ class PlayerHistoryController extends AbstractActionController
 			throw new \RuntimeException('You can only use this action from a console!');
 		}
 
-		$player = $request->getParam('player',0);
+		// run only 1 time, beta test
+		// @todo refactoring that part in a service
+		$lockHandler = new LockHandler($request->getContent()[0]);
+
+		if (!$lockHandler->lock()) {
+			throw new \RuntimeException('already running');
+		}
+
+		$player = $request->getParam('player', 0);
 
 		$this->getPlayerHistory()->setCurrentPlayer($player);
 	}
